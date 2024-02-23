62cadfe0305d4a41f65c63253fb300ab71cbfa0e
Nico Dhillon <nicdhillon19@gmail.com>
2024-02-23 14:12:01 +0000

    initial links added to navbar

--- nico_portfolio/app/_components/skipToMenu/SkipToMenu.tsx
+++ nico_portfolio/app/_components/skipToMenu/SkipToMenu.tsx
@@ -1,6 +1,14 @@
 import Link from "next/link"
 
 export default function SkipToMenu () {
-    return <Link href="#here">Search</Link>
-    
+    return (
+    <nav className=" basis-6 grow ">
+            <Link href="#top">Home</Link>
+            <Link href="#about">About</Link>
+            <Link href="#projects">Projects</Link>
+            <Link href="#experience">Experience</Link>
+            <Link href="#contact">Contact</Link>
+            <Link href="#download">Download CV</Link>
+    </nav>
+    )
 }
