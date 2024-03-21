e8c25fe572e8997df943d280d4904bf5d5d758aa
Nico Dhillon <nicdhillon19@gmail.com>
2024-03-21 15:28:26 +0000

    large breakpoint customised for responsiveness

--- nico_portfolio/app/_components/menu/Menu.tsx
+++ nico_portfolio/app/_components/menu/Menu.tsx
@@ -2,14 +2,15 @@ import Link from "next/link"
 import Logo from "../logo/Logo"
 import Header from "../header/Header"
 
+
 export default function Menu () {
     return (
-    <nav className="border-2 border-web-orange-600 bg-purple-heart-600 text-purple-heart-50 top-1 fixed flex sm:flex-row flex-col sm:w-screen sm:h-auto h-screen w-auto justify-between">
+    <nav className="border-2 border-web-orange-600 bg-purple-heart-600 text-purple-heart-50 top-1 fixed flex lg:flex-row flex-col lg:w-screen lg:h-auto h-screen w-auto justify-between">
         <section className="flex flex-row p-2">
         <Logo />
         <Header />
         </section>
-            <section className="flex sm:text-3xl text-xl sm:flex-row flex-col shrink gap-5" id="nav-links">
+            <section className="flex lg:text-3xl text-xl lg:flex-row flex-col shrink gap-5" id="nav-links">
                 <Link href="#top">Home</Link>
                 <Link href="#about">About</Link>
                 <Link href="#projects">Projects</Link>
