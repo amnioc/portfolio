4cfc11bfbdfba64ed79419f8b5fec7c9a14922c8
Nico Dhillon <nicdhillon19@gmail.com>
2024-04-09 16:47:36 +0100

    adds Project section filler text

--- nico_portfolio/app/_components/Projects.tsx
+++ nico_portfolio/app/_components/Projects.tsx
@@ -1,5 +1,36 @@
 export default function Projects() {
-    return (
-        <h3 id="projects">Projects</h3>
-    )
+  return (
+    <section
+      id="projects"
+      className="border-2 border-web-orange-600 text-purple-heart-900 p-10 mt-10 place-self-stretch"
+    >
+      <ul>
+        <h3 className="text-2xl">Projects</h3>
+        <li className="project">
+          <h4>Project 1</h4>
+          <a href="www.google.com">View Project 1 Here</a>
+          <aside>
+            Highlights included learning how to integrate memory recall!
+          </aside>
+          Tech Stack: A, B, C
+        </li>
+        <li className="project">
+          <h4>Project 2</h4>
+          <a href="www.google.com">View Project 2 Here</a>
+          <aside>
+            Highlights included learning how to integrate memory recall!
+          </aside>
+          Tech Stack: A, B, C
+        </li>
+        <li className="project">
+          <h4>Project 3</h4>
+          <a href="www.google.com">View Project 3 Here</a>
+          <aside>
+            Highlights included learning how to integrate memory recall!
+          </aside>
+          Tech Stack: A, B, C
+        </li>
+      </ul>
+    </section>
+  );
 }
