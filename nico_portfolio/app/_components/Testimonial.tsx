dca9bed9519e276d36730e0820380a7600995a31
Nico Dhillon <nicdhillon19@gmail.com>
2024-04-14 17:52:27 +0100

    rearranges assets to public for image visibility

--- nico_portfolio/app/_components/Testimonial.tsx
+++ nico_portfolio/app/_components/Testimonial.tsx
@@ -3,35 +3,35 @@ export default function Testimonial() {
 <section id="testimonials" className="snap-x border border-black flex p-4 overflow-x-auto">
     <section className="snap-center m-5">
         Testimonial 1
-        <img src="./icon.png" height={150} width={400} alt="black car" className="flex"></img>
+        <img src="./assets/images/cat.jpg" height={150} width={400} alt="black car" className="flex"></img>
     </section>
     <section className="snap-center m-5">
         Testimonial 1
-        <img src="./icon.png" height={150} width={400} alt="black car" className="flex"></img>
+        <img src="./assets/images/cat.jpg" height={150} width={400} alt="black car" className="flex"></img>
     </section>
     <section className="snap-center m-5">
         Testimonial 1
-        <img src="./icon.png" height={150} width={400} alt="black car" className="flex"></img>
+        <img src="./assets/images/cat.jpg" height={150} width={400} alt="black car" className="flex"></img>
     </section>
     <section className="snap-center m-5">
         Testimonial 1
-        <img src="./icon.png" height={150} width={400} alt="black car" className="flex"></img>
+        <img src="./assets/images/cat.jpg" height={150} width={400} alt="black car" className="flex"></img>
     </section>
     <section className="snap-center m-5">
         Testimonial 5
-        <img src="./icon.png" height={150} width={400} alt="black car" className="flex"></img>
+        <img src="./assets/images/cat.jpg" height={150} width={400} alt="black car" className="flex"></img>
     </section>
     <section className="snap-center m-5">
         Testimonial 5
-        <img src="./icon.png" height={150} width={400} alt="black car" className="flex"></img>
+        <img src="./assets/images/cat.jpg" height={150} width={400} alt="black car" className="flex"></img>
     </section>
     <section className="snap-center m-5">
         Testimonial 5
-        <img src="./icon.png" height={150} width={400} alt="black car" className="flex"></img>
+        <img src="./assets/images/cat.jpg" height={150} width={400} alt="black car" className="flex"></img>
     </section>
     <section className="snap-center m-5">
         Testimonial 5
-        <img src="./icon.png" height={150} width={400} alt="black car" className="flex"></img>
+        <img src="./assets/images/cat.jpg" height={150} width={400} alt="black car" className="flex"></img>
     </section>
 </section>    )
 }
--- nico_portfolio/app/_components/Welcome.tsx
+++ nico_portfolio/app/_components/Welcome.tsx
@@ -1,15 +1,26 @@
+import Image from "next/image";
 
-export default function Welcome () {
- return (
-    <section className="p-5 mt-10 border-solid border-pink-700 border-2 flex" id="top">
-        
-        <img src="./face.png" alt="Nico smiling" className="flex-initial rounded-xl w-50 h-50 border-purple-600 border-2"></img>
-        <aside className="p-5">
-         <h2 className=" text-purple-800 font-extrabold text-2xl">Hi,</h2>   
-        I'm a Junior Front End Developer with a passion for inclusion of <i className="text-l">all</i> users.
-        <aside className="text-purple-heart-800 decoration-2">Welcome to my portfolio page.</aside>
+export default function Welcome() {
+  return (
+    <section
+      className="p-5 mt-10 border-solid border-pink-700 border-2 flex"
+      id="top"
+    >
+      <img
+        src="../assets/images/nico.jpg"
+        alt="Nico smiling"
+        width={150}
+        height={150}
+        className="rounded-2xl p-3"
+      />
+      <aside className="p-5">
+        <h2 className=" text-purple-800 font-extrabold text-2xl">Hi,</h2>
+        I'm a Junior Front End Developer with a passion for inclusion of{" "}
+        <i className="text-l">all</i> users.
+        <aside className="text-purple-heart-800 decoration-2">
+          Welcome to my portfolio page.
         </aside>
+      </aside>
     </section>
-    )
- 
+  );
 }
