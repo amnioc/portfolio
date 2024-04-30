568a49135bbf5a4fe5c2065ed37a351abaceed85
Nico Dhillon <nicdhillon19@gmail.com>
2024-04-30 18:07:33 +0100

    adds first experience text

--- nico_portfolio/app/_components/Experience.tsx
+++ nico_portfolio/app/_components/Experience.tsx
@@ -7,15 +7,14 @@ export default function Experience() {
       <ol>
         <h3 className="text-2xl">Experience</h3>
         <li className="experience border-2 border-purple-heart-600 p-5 mt-3">
-          <h4>Job Title 1</h4>
+          <h4 className="text-xl font-semibold text-purple-heart-700">Freelance Software Developer</h4>
+          <aside className="text-l font-bold text-web-orange-700">Remote, 2023</aside>
           <section className="mt-3">
-            Date
-            <br></br> Location
             <ul className="list-disc ml-5">
-              <li>Task 1</li>
-              <li>Task 2</li>
-              <li>Task 3</li>
-              <li>Task 4</li>
+              <li>Developed engaging donation form for local charity with Customer Relationship Management (CRM) integration.</li>
+              <li>Communicated with client to understand and identify minimum viable product (MVP).</li>
+              <li>Provided regular updates over email and video call, managing expectations whilst prioritising user experiene.</li>
+              <li>Continued professional development, self-learning new technologies and researching best options to implement.</li>
             </ul>
           </section>
         </li>
